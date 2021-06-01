7d5288d8a24dcdb95af320bb57171c206cec328d
md-sharfuzzaman <msharfuzzaman9@gmail.com>
2021-06-01 08:42:07 +0600

    animated gallary

---
+++ app.js
@@ -0,0 +1,29 @@
+let btn = document.getElementsByClassName("btn");
+let banner = document.getElementById("banner");
+btn[0].onclick = function(){
+    banner.src = "images/pic1.png";
+
+    animation()
+    this.classList.add("active")
+}
+btn[1].onclick = function(){
+    banner.src = "images/pic2.png";
+    animation()
+    this.classList.add("active")
+}
+btn[2].onclick = function(){
+    banner.src = "images/pic3.png";
+    animation()
+    this.classList.add("active")
+}
+
+function animation(){
+    banner.classList.add("zoom");
+    setTimeout(() => {
+        banner.classList.remove("zoom");
+    }, 500);
+    for(b of btn){
+        b.classList.remove("active");
+    }
+}
+
